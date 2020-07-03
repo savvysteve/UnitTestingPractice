54aa80814a1b1fdd03f0df959f24ad9a0c50b766
Steven Johnson <stevenrjohnson@gmail.com>
2020-07-03 00:56:55 -0500

    Added two more tests on Length Conversion.

--- tests/test_LengthConversion_UnitTest.py
+++ tests/test_LengthConversion_UnitTest.py
@@ -9,7 +9,11 @@ def test_ConvertInchestoMMExample1():
 def test_ConvertInchestoMMExample2():
     Result = ConvertInchestoMM(3.0625).__round__(7)
     assert Result == 77.7875
-    
+
+def test_ConvertInchestoMMExample3():
+    Result = ConvertInchestoMM(7.775).__round__(7)
+    assert Result == 197.485
+
 def test_ConvertMMtoInchesExample1():
     Result = ConvertMMtoInches(1).__round__(7)
     assert Result == 0.0393701
@@ -17,5 +21,9 @@ def test_ConvertMMtoInchesExample1():
 def test_ConvertMMtoInchesExample2():
     Result = ConvertMMtoInches(47).__round__(7)
     assert Result == 1.8503937
+
+def test_ConvertMMtoInchesExample3():
+    Result = ConvertMMtoInches(7.775).__round__(8)
+    assert Result == 0.30610236
 
 
